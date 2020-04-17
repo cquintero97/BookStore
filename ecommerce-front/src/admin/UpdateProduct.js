c556182bb72b884f0898d17b79b0aae60130419f
Christian Quintero <cdquintero7@gmail.com>
2020-04-16 19:43:52 -0700

    update product fixes
    - allowed "update product" page to show product category selected
    - added restrictions on input data for quantity and price

--- ecommerce-front/src/admin/UpdateProduct.js
+++ ecommerce-front/src/admin/UpdateProduct.js
@@ -3,8 +3,21 @@ import Layout from '../core/Layout'
 import {isAuthenticated} from '../auth'
 import {Link, Redirect} from 'react-router-dom'
 import {getProduct, getCategories, updateProduct} from './apiAdmin'
+import {read} from '../core/apiCore'
 
 const UpdateProduct = ({match}) => {
+  const [product, setProduct] = useState({})
+    const [err, setErr] = useState(false)
+
+  const loadSingleProduct = productId => {
+    read(productId).then(data => {
+      if(data.err) {
+        setErr(data.err)
+      } else {
+        setProduct(data)
+      }
+    })
+  }
 
   const [values, setValues] = useState({
     name: '',
@@ -72,13 +85,24 @@ const UpdateProduct = ({match}) => {
   }
 
   useEffect(() => {
-    init(match.params.productId)
+    const productId = match.params.productId
+    init(productId)
+    loadSingleProduct(productId)
   }, [])
 
   const handleChange = name => event => {
     const value = name === 'photo' ? event.target.files[0] : event.target.value
-    formData.set(name, value)
-    setValues({...values, [name]: value})
+    if (name == 'price') {
+      formData.set(name, parseFloat(value).toFixed(2))
+      setValues({...values, [name]: value})
+    } else if (name == 'quantity') {
+      formData.set(name, parseFloat(value).toFixed(0))
+      setValues({...values, [name]: parseFloat(value).toFixed(0)})
+    } else {
+      formData.set(name, value)
+      setValues({...values, [name]: value})
+    }
+
   }
 
   const clickSubmit = event => {
@@ -105,7 +129,20 @@ const UpdateProduct = ({match}) => {
     })
   }
 
-  const newPostForm = () => (
+  const showOptions = (c, i) => {
+    if (c.name == product.category.name) {
+      return(
+        <option key={i} value={c._id} selected>{c.name}</option>
+      )
+    } else {
+      return(
+        <option key={i} value={c._id}>{c.name}</option>
+      )
+    }
+  }
+
+  const newPostForm = () => {
+    return(
     <form className="mb-3" onSubmit={clickSubmit}>
       <h4>Post Photo</h4>
       <div className="form-group">
@@ -126,7 +163,7 @@ const UpdateProduct = ({match}) => {
 
       <div className="form-group">
         <label className="text-muted">Price</label>
-        <input onChange={handleChange('price')} type="number" className="form-control" value={price} />
+        <input onChange={handleChange('price')} type="number" step="0.01" className="form-control" value={price} />
       </div>
 
       <div className="form-group">
@@ -134,7 +171,7 @@ const UpdateProduct = ({match}) => {
         <select onChange={handleChange('category')} className="form-control" >
 
           <option>Please select</option>
-          {categories && categories.map((c, i) => (<option key={i} value={c._id}>{c.name}</option>))}
+          {categories && categories.map((c, i) => showOptions(c,i))}
 
         </select>
       </div>
@@ -144,18 +181,18 @@ const UpdateProduct = ({match}) => {
         <select onChange={handleChange('shipping')} className="form-control" >
           <option>Please select</option>
           <option value="0">No</option>
-          <option value="1">yes</option>
+          <option value="1" selected>yes</option>
         </select>
       </div>
 
       <div className="form-group">
         <label className="text-muted">Quantity</label>
-        <input onChange={handleChange('quantity')} type="number" className="form-control" value={quantity} />
+        <input onChange={handleChange('quantity')} type="number" step="1" className="form-control" required value={quantity} />
       </div>
 
       <button className="btn btn-outline-primary">Update Product</button>
     </form>
-  )
+  )}
 
   const showError = () => (
     <div className="alert alert-danger" style={{display: error ? '' : 'none'}}>
